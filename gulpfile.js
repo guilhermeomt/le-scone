fec9dff3f9803da79ff1da13fab979e8d09e9504
Guilherme Tavares <guilhermet213@gmail.com>
2021-02-19 10:31:08 -0300

    Set up Browsersync and watcher tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const gulpStylelint = require("gulp-stylelint");
+const browserSync = require("browser-sync").create();
 
 const lintSass = () => {
   return gulp.src("css/scss/*.scss").pipe(
@@ -22,8 +23,25 @@ const buildSass = () => {
     .pipe(autoprefixer({ overrideBrowserslist: ["last 3 versions"] }))
     .pipe(rename({ suffix: ".min" }))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("css"));
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
+};
+
+const watch = () => {
+  gulp.watch("css/scss/*.scss", gulp.series(["lint:sass", "sass"]));
+  gulp.watch("./*.html").on('change', browserSync.reload);
+};
+
+const server = () => {
+  browserSync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
 };
 
 gulp.task("lint:sass", lintSass);
 gulp.task("sass", buildSass);
+gulp.task("watch", watch);
+gulp.task("server", server);
+gulp.task("default", gulp.parallel("watch", "server"));
